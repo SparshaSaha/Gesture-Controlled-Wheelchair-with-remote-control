e0fd50d48d7c77b8e11446e42ddf7fd362574c02
sparsha <saha.sparsha@gmail.com>
2018-12-14 21:34:03 +0530

    Comments added

--- Server/server.js
+++ Server/server.js
@@ -5,8 +5,10 @@ const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
 
+// Create Database Instance and connect to database
 var database = mongoose.connection.openUri('mongodb://127.0.0.1:27017/WheelChairDatabase');
 
+// On connection successful
 mongoose.connection.once('connected', function() {
     console.log("Connected to Database");
 });
