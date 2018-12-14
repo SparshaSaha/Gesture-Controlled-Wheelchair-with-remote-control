0d9149ddad8e2ad6a6686a0f214653f3aa2c2fda
sparsha <saha.sparsha@gmail.com>
2018-12-14 21:23:13 +0530

    database control added

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,14 @@
 "use strict";
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 3000;
+
+var database = mongoose.connection.openUri('mongodb://127.0.0.1:27017/WheelChairDatabase');
+
+mongoose.connection.once('connected', function() {
+    console.log("Connected to Database");
+});
 
 app.listen(port);
