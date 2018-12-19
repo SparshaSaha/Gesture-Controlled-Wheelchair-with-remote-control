ab46d3d3286cb1174787b985907582e0d5a0b375
sparsha <saha.sparsha@gmail.com>
2018-12-20 01:49:15 +0530

    Updates

--- Server/server.js
+++ Server/server.js
@@ -2,10 +2,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const port = 3000;
+const port = 8010;
+const databaseHandler = require('./Modules/databaseHandler.js');
 
 // Create Database Instance and connect to database
-var database = mongoose.connection.openUri('mongodb://127.0.0.1:27017/WheelChairDatabase');
+var database = mongoose.connection.openUri('mongodb://127.0.0.1:27017/WheelChairDatabase4');
 
 // On connection successful
 mongoose.connection.once('connected', function() {
