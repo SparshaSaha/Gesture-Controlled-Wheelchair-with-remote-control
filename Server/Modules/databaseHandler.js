53f2e7272caa418eb6a2e138b5ba05b4061c13db
sparsha <saha.sparsha@gmail.com>
2018-12-20 01:49:42 +0530

    insertion logic added

--- Server/Modules/databaseHandler.js
+++ Server/Modules/databaseHandler.js
@@ -1,11 +1,34 @@
 "use strict"
+const Location = require('./../DatabaseModels/Location');
+
 module.exports = {
 
-    insertIntoDatabase: function(database, wheelChairData) {
-      
-    },
+    insertOrUpdateLocation: function(location) {
 
-    updateLocation: function(database, location) {
+        // Check if a previous location object is present
+        Location.find({}, (error, response) => {
 
+            if (!error) {
+
+                if (response.length == 0) {
+                    
+                    // If database has no previous save location object
+                    var wheelChairLocation = new Location({
+                        latitude: 14.335,
+                        longitude: 15.667,
+                        timeStamp: 'RandomTime'
+                    });
+
+                    wheelChairLocation.save((err, resp) => {
+                        if(!err) {
+                            console.log("Saved");
+                        }
+                    });
+                } else {
+                    // Update
+                }
+            }
+
+        });
     }
 };
