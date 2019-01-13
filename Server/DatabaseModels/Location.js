ab197b81584f7b16457cb6586f345c253271e71c
sparsha <saha.sparsha@gmail.com>
2019-01-13 18:48:50 +0530

    Location model changed as suggested

--- Server/DatabaseModels/Location.js
+++ Server/DatabaseModels/Location.js
@@ -7,7 +7,7 @@ var locationSchema = new mongoose.Schema({
     latitude: {type: SchemaTypes.Double, required: true},
     longitude: {type: SchemaTypes.Double, required: true},
     accuracy: {type: SchemaTypes.Double},
-    timeStamp: {type:String, required: true}
+    timeStamp: {type: Date, default: Date.now(), required: true}
 });
 
 module.exports = mongoose.model('Location', locationSchema);
