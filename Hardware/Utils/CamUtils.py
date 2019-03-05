7af71d12ca9a42a37f5e365c7dc45cbdf8733025
Sparsha Saha <saha.sparsha@gmail.com>
2019-03-05 19:12:46 +0530

    stopper added and exception handler added

--- Hardware/Utils/CamUtils.py
+++ Hardware/Utils/CamUtils.py
@@ -6,22 +6,31 @@ import time
 
 class WebCamUsingPhone(threading.Thread):
   
-  def __init__(self, threadID, name, url):
+  def __init__(self, threadID, name, url, stopper):
     threading.Thread.__init__(self)
     self.threadID = threadID
     self.name = name
     self.url = url
+    self.stopper = stopper
 
   def run(self):
-    while True:
-      capturedFrame = urlopen(self.url)
-      frameAsArray = np.array(bytearray(capturedFrame.read()), dtype=np.uint8)
-      decodedImage = cv2.imdecode(frameAsArray, -1)
-      cv2.imshow(self.name, decodedImage)
+    while not self.stopper.is_set():
+      try:
+        capturedFrame = urlopen(self.url)
+        frameAsArray = np.array(bytearray(capturedFrame.read()), dtype=np.uint8)
+        decodedImage = cv2.imdecode(frameAsArray, -1)
+        cv2.imshow(self.name, decodedImage)
+      except Exception as e:
+        print('Exception occured....exiting')
+        exit(0)
+        
       # Export through socket
       if ord('q') == cv2.waitKey(10):
         exit(0)
+        
+      exit(0)
 
-
-threadTest = WebCamUsingPhone(1, 'front', "http://10.105.16.217:8080/shot.jpg")
+stopper = threading.Event()
+threadTest = WebCamUsingPhone(1, 'left', "http://10.105.17.33:8080/shot.jpg", stopper)
 threadTest.start()
+
