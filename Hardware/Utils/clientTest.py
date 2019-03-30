7eb451fd3fd0a599d9f9491f7158e687f0b59625
sparsha <saha.sparsha@gmail.com>
2019-03-31 00:34:07 +0530

    clientSend tested

--- Hardware/Utils/clientTest.py
+++ Hardware/Utils/clientTest.py
@@ -2,6 +2,8 @@ import socket
 from SocketReadUtils import SocketReadUtils
 import threading
 from CallBackHandler import CallBackHandler
+import json
+from SendUtils import SendUtils
 
 def call1():
     print("Callback 1")
@@ -11,11 +13,15 @@ def call2():
 
 def main():
     soc = socket.socket()
-    soc.connect(('127.0.0.1', 2002))
+    soc.connect(('127.0.0.1', 2000))
     x = SocketReadUtils(soc, 'reader', threading.Event())
     x.registerCallback(CallBackHandler('a', call1))
     x.registerCallback(CallBackHandler('b', call2))
     x.start()
+    arr = [[1,2,3], [3,4,5]]
+    arrJson = json.dumps(arr)
+    s = SendUtils(soc, arrJson)
+    s.start()
     while True:
         pass
 
